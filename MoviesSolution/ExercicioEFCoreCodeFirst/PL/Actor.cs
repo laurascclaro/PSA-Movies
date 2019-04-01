6fbdfc43c06369e9452638a37901e32fd40121ee
laurascclaro <laurascclaro@hotmail.com>
2019-04-01 20:55:49 -0300

    aula 01/04

---
+++ MoviesSolution/ExercicioEFCoreCodeFirst/PL/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _ExercicioEFCoreCodeFirst.PL
+{
+    class Actor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        
+
+       
+    }
+}
---
+++ MoviesSolution/ExercicioEFCoreCodeFirst/PL/ActorMovie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _ExercicioEFCoreCodeFirst.PL
+{
+    class ActorMovie
+    {
+        public int ActorMovieID { get; set; }
+        public string Character { get; set; }
+
+        public int ID { get; set; }//id do movie
+        public Movie Movie { get; set; }
+
+        public int ActorID{ get; set; }
+        public Actor Actor { get; set; }
+        
+
+
+    }
+}
